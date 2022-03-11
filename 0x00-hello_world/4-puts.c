fe910cd7d759cc8e0a8be28746caf86ed48c8e4f
Inioluwa <inioluwaolarinoye4@gmail.com>
2022-03-10 16:52:44 -0800

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-int main (void)
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"):
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
