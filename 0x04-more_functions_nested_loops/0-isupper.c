434ae3524dbf59017e93d3927686028e07e1fa09
Inioluwa <inioluwaolarinoye4@gmail.com>
2022-03-18 17:34:46 -0700

    task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - evalue if letter is uppercase .
+ *@c: pront int
+ *Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= '2')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
