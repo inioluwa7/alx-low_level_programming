f090b92e5304c3508d1d1b1a4da528ec9aff843a
Inioluwa <inioluwaolarinoye4@gmail.com>
2022-03-21 21:20:29 -0700

    test

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *main
+ *
+ *
+ *Return: void
+ */
+
+void puts_half(char *str)
+
+{
+int i, last;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++
+}
+
+last = (i + 1) / 2;
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+_putchar ('\n');
+}
