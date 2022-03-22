0c59e15a53a668a321f30ac895c16260c8dbc5e8
Inioluwa <inioluwaolarinoye4@gmail.com>
2022-03-21 20:24:19 -0700

    message

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates a integer value to 98
+ *@n: Apointer to an int that will be updated
+ *
+ *Return: void that means in correct
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
