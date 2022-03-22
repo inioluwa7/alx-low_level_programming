b8d9809649e3c3ef197753057e50f57e10b9ed0d
Inioluwa <inioluwaolarinoye4@gmail.com>
2022-03-21 21:14:02 -0700

    test

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar (str[a]);
+}
+
+_putchar ('\n');
+}
