3ae2b47ea3760c41e14158f4922ea3977de29c53
Inioluwa <inioluwaolarinoye4@gmail.com>
2022-03-21 21:35:54 -0700

    test

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcpy - copies the str
+ *@dest: a pointer
+ *@src: a pointer
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+int i;
+
+i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
