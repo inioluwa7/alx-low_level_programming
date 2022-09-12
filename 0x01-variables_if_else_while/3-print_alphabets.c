b97e2aa105f73a56f99e4b5370cc5ba31eccda48
Olarinoye Ayodeji Inioluwa <inioluwaolarinoye4@gmail.com>
2022-09-12 16:36:52 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - program to print in all cases then new line
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		letter = tolower(letter);
+		putchar(letter);
+		if(letter == 'z')
+		{
+			letter = 'A';
+			for (; letter <= 'Z'; letter++)
+			{
+				putchar(letter);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
