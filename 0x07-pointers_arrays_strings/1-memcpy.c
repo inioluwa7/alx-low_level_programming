9c35cbd957efe91b6272f24a5384c302b1e4399e
Olarinoye Ayodeji Inioluwa <inioluwaolarinoye4@gmail.com>
2022-09-27 19:11:39 -0700

    pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,16 +3,16 @@
 /**
  * _memcpy - irontnoux2
  * @dest: pointer to char params
- * @src: pointer to char 
+ * @src: pointer to char params
  * @n: size
- * return: *dest
+ * Return: *dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
